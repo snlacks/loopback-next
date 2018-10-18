8cdf13a87369a0e30bf763c66ce9730cdf4e0a7c
Raymond Feng <enjoyjava@gmail.com>
2018-10-17 17:51:01 -0700

    feat(rest): switch to express body-parser

--- packages/rest/test/integration/http-handler.integration.ts
+++ packages/rest/test/integration/http-handler.integration.ts
@@ -293,9 +293,9 @@ describe('HttpHandler', () => {
         .send('key=' + givenLargeRequest())
         .expect(413, {
           error: {
-            message: 'request entity too large',
-            name: 'Error',
             statusCode: 413,
+            name: 'PayloadTooLargeError',
+            message: 'request entity too large',
           },
         })
         .catch(ignorePipeError)
@@ -310,9 +310,9 @@ describe('HttpHandler', () => {
         .send({key: givenLargeRequest()})
         .expect(413, {
           error: {
-            message: 'request entity too large',
-            name: 'Error',
             statusCode: 413,
+            name: 'PayloadTooLargeError',
+            message: 'request entity too large',
           },
         })
         .catch(ignorePipeError)
@@ -323,7 +323,7 @@ describe('HttpHandler', () => {
       const body = {key: givenLargeRequest()};
       rootContext
         .bind(RestBindings.REQUEST_BODY_PARSER_OPTIONS)
-        .to({limit: 4 * 1024 * 1024}); // Set limit to 4MB
+        .to({limit: '4mb'}); // Set limit to 4MB
       return client
         .post('/show-body')
         .set('content-type', 'application/json')
